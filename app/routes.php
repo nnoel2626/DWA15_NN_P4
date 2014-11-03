027201222ee3ea87ff4187bf6408fe11fcfed754
NORCIUS NOEl <nnoel@harvard.edu>
2014-11-02 22:02:01 -0500

    database set-up

--- app/routes.php
+++ app/routes.php
@@ -34,19 +34,19 @@ Route::get('/', function()
 });
 
 
-Route::get('mysql-test', function() {
+// Route::get('mysql-test', function() {
 
-    # Print environment
-    echo 'Environment: '.App::environment().'<br>';
+//     # Print environment
+//     echo 'Environment: '.App::environment().'<br>';
 
 
-    # Use the DB component to select all the databases
-    $results = DB::select('SHOW DATABASES;');
+//     # Use the DB component to select all the databases
+//     $results = DB::select('SHOW DATABASES;');
 
-    # If the "Pre" package is not installed, you should output using print_r instead
-    echo Pre::render($results);
+//     # If the "Pre" package is not installed, you should output using print_r instead
+//     echo Pre::render($results);
 
-});
+// });
 
 # /app/routes.php
 Route::get('/debug', function() {
